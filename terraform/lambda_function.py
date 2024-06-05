6abdb2cb7da6066b3e71b5d5106ff5fb16c63466
Maryna <127458923+marnka@users.noreply.github.com>
2024-06-05 18:28:39 +0300

    Update lambda_function.py

--- terraform/lambda_function.py
+++ terraform/lambda_function.py
@@ -4,10 +4,19 @@ import os
 s3 = boto3.client('s3')
 
 def lambda_handler(event, context):
-    source_bucket = os.environ['SOURCE_BUCKET']
-    dest_bucket = os.environ['DEST_BUCKET']
-
+    source_bucket = 's3-start'
+    destination_bucket = 's3-finish'
+    
     for record in event['Records']:
         key = record['s3']['object']['key']
-        copy_source = {'Bucket': source_bucket, 'Key': key}
-        s3.copy_object(CopySource=copy_source, Bucket=dest_bucket, Key=key)
+        
+        copy_source = {
+            'Bucket': source_bucket,
+            'Key': key
+        }
+
+        try:
+            s3.copy_object(CopySource=copy_source, Bucket=destination_bucket, Key=key)
+            print(f'Successfully copied {key} from {source_bucket} to {destination_bucket}')
+        except Exception as e:
+            print(f'Error copying {key}: {str(e)}')
