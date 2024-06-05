2f4a3c755c53cc88eda5f39e29f3c62e20603864
Maryna <127458923+marnka@users.noreply.github.com>
2024-06-05 18:27:49 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,5 @@
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
-
-  endpoints {
-    s3      = "http://localhost:4566"
-    lambda  = "http://localhost:4566"
-  }
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "s3_start" {
@@ -16,11 +10,11 @@ resource "aws_s3_bucket" "s3_finish" {
   bucket = "s3-finish"
 }
 
-resource "aws_s3_bucket_lifecycle_configuration" "example" {
+resource "aws_s3_bucket_lifecycle_configuration" "s3_start_lifecycle" {
   bucket = aws_s3_bucket.s3_start.id
 
   rule {
-    id     = "expire-logs"
+    id     = "expire_logs"
     status = "Enabled"
 
     expiration {
@@ -29,26 +23,77 @@ resource "aws_s3_bucket_lifecycle_configuration" "example" {
   }
 }
 
-resource "aws_lambda_function" "copy_file" {
-  filename      = "lambda.zip"
-  function_name = "copyFileFunction"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  role          = "arn:aws:iam::000000000000:role/lambda-role"
+data "aws_iam_policy_document" "lambda_policy" {
+  statement {
+    actions = [
+      "s3:GetObject",
+      "s3:PutObject",
+    ]
 
-  environment {
-    variables = {
-      SOURCE_BUCKET = aws_s3_bucket.s3_start.bucket
-      DEST_BUCKET   = aws_s3_bucket.s3_finish.bucket
-    }
+    resources = [
+      "${aws_s3_bucket.s3_start.arn}/*",
+      "${aws_s3_bucket.s3_finish.arn}/*",
+    ]
+  }
+
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+    ]
+
+    resources = ["arn:aws:logs:*:*:*"]
   }
 }
 
-resource "aws_s3_bucket_notification" "s3_start_notification" {
+resource "aws_iam_role" "lambda_role" {
+  name               = "lambda_exec_role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "lambda_policy" {
+  name   = "lambda_policy"
+  role   = aws_iam_role.lambda_role.id
+  policy = data.aws_iam_policy_document.lambda_policy.json
+}
+
+resource "aws_lambda_function" "s3_copy_function" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "s3_copy_function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
+
+resource "aws_lambda_permission" "allow_s3_invoke" {
+  statement_id  = "AllowExecutionFromS3"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_copy_function.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.s3_start.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
   bucket = aws_s3_bucket.s3_start.id
 
   lambda_function {
-    lambda_function_arn = aws_lambda_function.copy_file.arn
+    lambda_function_arn = aws_lambda_function.s3_copy_function.arn
     events              = ["s3:ObjectCreated:*"]
   }
 }
