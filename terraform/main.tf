f35a1cc6140495845fb40c32b427bc3764f58f8b
Maryna <127458923+marnka@users.noreply.github.com>
2024-06-08 12:00:35 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,22 @@
 provider "aws" {
-  region = "us-east-1"
+
+  access_key = AKIAZQ3DRVVK7L2XAVW2
+  secret_key = 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"
+  region = "ua-east-1"
+
+  s3_use_path_style  = true
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
+
+  endpoints {
+    s3 = "http://localhost:4566"
+    lambda = "http://localhost:4566"
+    sts = "http://localhost:4566"
+    iam = "http://localhost:4566"
+    sns = "http://localhost:4566"
+    sqs = "http://localhost:4566"
+  }
 }
 
 resource "aws_s3_bucket" "s3_start" {
